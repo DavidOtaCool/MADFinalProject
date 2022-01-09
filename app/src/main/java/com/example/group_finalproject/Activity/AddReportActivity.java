bd5010183ea7cd39742dbbc8f89618fd7fcc0f6e
David A <DavidOtaCool@users.noreply.github.com>
2022-01-09 17:35:54 +0700

    MAD Final Project 20220109-2 (Add dan edit pakai spinner gitu)

--- app/src/main/java/com/example/group_finalproject/Activity/AddReportActivity.java
+++ app/src/main/java/com/example/group_finalproject/Activity/AddReportActivity.java
@@ -1,10 +1,19 @@
 package com.example.group_finalproject.Activity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +22,8 @@ import com.example.group_finalproject.API.APIRequestPostData;
 import com.example.group_finalproject.API.PostRetroServer;
 import com.example.group_finalproject.Model.PostResponseModel;
 import com.example.group_finalproject.R;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,7 +34,11 @@ public class AddReportActivity extends AppCompatActivity {
     private EditText etCategory, etTextReport, etLocation, etAuthor;
     private Button btnAddReport;
     private String postCategory, postTextReport, postLocation, postAuthor;
+    String categoryPosition = "";
 
+    TextView selectCategory;
+    ArrayList<String> categoryList;
+    Dialog categoryDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +47,65 @@ public class AddReportActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_add_report);
 
-        etCategory = findViewById(R.id.et_category);
+        selectCategory = findViewById(R.id.tv_select_category);
+
+        categoryList = new ArrayList<>();
+        categoryList.add("Accident");
+        categoryList.add("Criminal");
+        categoryList.add("Infrastructure");
+        categoryList.add("Environment");
+
+        selectCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                categoryDialog = new Dialog(AddReportActivity.this);
+
+                categoryDialog.setContentView(R.layout.category_selection);
+
+                categoryDialog.getWindow().setLayout(700, 800);
+
+                categoryDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                categoryDialog.show();
+
+                EditText etSearchCategory = categoryDialog.findViewById(R.id.et_search_category);
+                ListView lvCategory = categoryDialog.findViewById(R.id.list_view_category);
+
+                ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(AddReportActivity.this,
+                        android.R.layout.simple_list_item_1,categoryList);
+
+                lvCategory.setAdapter(categoryAdapter);
+
+                etSearchCategory.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        categoryAdapter.getFilter().filter(s);
+
+                    }
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+
+                lvCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        selectCategory.setText(categoryAdapter.getItem(position));
+                        categoryPosition = selectCategory.getText().toString();
+                        selectCategory.setError(null);
+                        selectCategory.setTextColor(Color.BLACK);
+                        categoryDialog.dismiss();
+                    }
+                });
+            }
+        });
+
+//        etCategory = findViewById(R.id.et_category);
         etTextReport = findViewById(R.id.et_text_report);
         etLocation = findViewById(R.id.et_location);
         etAuthor = findViewById(R.id.et_author);
@@ -42,18 +115,30 @@ public class AddReportActivity extends AppCompatActivity {
         btnAddReport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                postCategory = etCategory.getText().toString();
+                postCategory = selectCategory.getText().toString();
                 postTextReport = etTextReport.getText().toString();
                 postLocation = etLocation.getText().toString();
                 postAuthor = etAuthor.getText().toString();
 
-                if(postCategory.trim().equals("")){
-
-                    etCategory.setError("Must input the category");
-
-                }else if(postTextReport.trim().equals("")){
+//
+//                if(postCategory.trim().equals("")){
+//
+//                    etCategory.setError("Must input the category");
+//
+//                }
+//                else if(postCategory.trim().equals("Select Category")){
+//                    selectCategory.setError("Must select the category");
+//
+//                }
+                if(categoryPosition.trim().equals("")){
+                    selectCategory.setError("");
+                    selectCategory.setTextColor(Color.RED);
+                    selectCategory.setText("Must select the category");
+                }
+                else if(postTextReport.trim().equals("")){
 
                     etTextReport.setError("Must input the text report");
+//                    selectCategory.setText(categoryAdapter.getItem(position));
 
                 }else if(postLocation.trim().equals("")){
 
--- app/src/main/java/com/example/group_finalproject/Activity/EditReportActivity.java
+++ app/src/main/java/com/example/group_finalproject/Activity/EditReportActivity.java
@@ -2,17 +2,28 @@ package com.example.group_finalproject.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.group_finalproject.API.APIRequestPostData;
 import com.example.group_finalproject.API.PostRetroServer;
 import com.example.group_finalproject.Model.PostResponseModel;
 import com.example.group_finalproject.R;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,11 +35,71 @@ public class EditReportActivity extends AppCompatActivity {
     private String getUpdatedPostAuthor, getUpdatedPostCategory, getUpdatedPostTextReport, getUpdatedPostLocation;
     private EditText etAuthor, etCategory, etTextReport, etLocation;
     private Button btnUpdate;
+
+    TextView selectCategory;
+    ArrayList<String> categoryList;
+    Dialog categoryDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_edit_post);
+
+        selectCategory = findViewById(R.id.tv_select_category2);
+
+        categoryList = new ArrayList<>();
+        categoryList.add("Accident");
+        categoryList.add("Criminal");
+        categoryList.add("Infrastructure");
+        categoryList.add("Environment");
+
+        selectCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                categoryDialog = new Dialog(EditReportActivity.this);
+
+                categoryDialog.setContentView(R.layout.category_selection);
+
+                categoryDialog.getWindow().setLayout(650, 800);
+
+                categoryDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                categoryDialog.show();
+
+                EditText etSearchCategory = categoryDialog.findViewById(R.id.et_search_category);
+                ListView lvCategory = categoryDialog.findViewById(R.id.list_view_category);
+
+                ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(EditReportActivity.this,
+                        android.R.layout.simple_list_item_1,categoryList);
+
+                lvCategory.setAdapter(categoryAdapter);
+
+                etSearchCategory.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        categoryAdapter.getFilter().filter(s);
+
+                    }
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+
+                lvCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        selectCategory.setText(categoryAdapter.getItem(position));
+                        categoryDialog.dismiss();
+                    }
+                });
+            }
+        });
 
         Intent receiveSelectedPostData = getIntent();
             //Sengaja dikasi defaultValue nya -1. Supaya bisa tahu, kalau kita dapat nilai id-nya -1 berarti ada yang salah.
@@ -39,14 +110,15 @@ public class EditReportActivity extends AppCompatActivity {
         receivedPostLocation = receiveSelectedPostData.getStringExtra("sentPostLocation");
 
         etAuthor = findViewById(R.id.et_author);
-        etCategory = findViewById(R.id.et_category);
+//        etCategory = findViewById(R.id.et_category);
         etTextReport = findViewById(R.id.et_text_report);
         etLocation = findViewById(R.id.et_location);
 
         btnUpdate = findViewById(R.id.btn_edit_report);
 
         etAuthor.setText(receivedPostAuthor);
-        etCategory.setText(receivedPostCategory);
+//        etCategory.setText(receivedPostCategory);
+        selectCategory.setText(receivedPostCategory);
         etTextReport.setText(receivedPostTextReport);
         etLocation.setText(receivedPostLocation);
 
@@ -54,7 +126,7 @@ public class EditReportActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 getUpdatedPostAuthor = etAuthor.getText().toString();
-                getUpdatedPostCategory = etCategory.getText().toString();
+                getUpdatedPostCategory = selectCategory.getText().toString();
                 getUpdatedPostTextReport = etTextReport.getText().toString();
                 getUpdatedPostLocation = etLocation.getText().toString();
                 updatePostData();
